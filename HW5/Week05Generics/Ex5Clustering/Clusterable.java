fab9046a107f854ef52b43abae8736dbc38d8786
akivaab <amabramo18@gmail.com>
2021-11-14 15:47:49 +0200

    now fixed?

--- HW5/Week05Generics/Ex5Clustering/Clusterable.java
+++
@@ -1,5 +0,0 @@
-package Week05Generics.Ex5Clustering;
-
-public interface Clusterable<T> {
-	public double distance(T other);
-}
--- HW5/Week05Generics/Ex5Clustering/Clustering.java
+++
@@ -1,7 +0,0 @@
-package Week05Generics.Ex5Clustering;
-
-import java.util.Set;
-
-public interface Clustering<T extends Clusterable<T>> {
-	public Set<Set<T>> clusterSet(Set<T> elements);
-}
