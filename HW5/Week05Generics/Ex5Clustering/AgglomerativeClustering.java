565515312ef6d73754ca0368269c3903f4d11adb
ronibuchine <ronibuchine@gmail.com>
2021-11-14 15:35:44 +0200

    fix again?

---
+++ HW5/Week05Generics/Ex5Clustering/AgglomerativeClustering.java
@@ -0,0 +1,16 @@
+package Week05Generics.Ex5Clustering;
+
+import java.util.Set;
+
+public class AgglomerativeClustering<T extends Clusterable<T>> implements Clustering<T> {
+	double threshold;
+
+	public AgglomerativeClustering(double threshold) {
+		this.threshold = threshold;
+	}
+
+	public Set<Set<T>> clusterSet(Set<T> elements) {
+		// TODO: Complete
+		return null;
+	}
+}
