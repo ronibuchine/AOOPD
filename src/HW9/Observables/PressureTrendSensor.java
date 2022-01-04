77eb30816db84e451ecce543f225291eaae02f98
akivaab <amabramo18@gmail.com>
2022-01-04 17:30:10 +0200

    added default values

--- src/HW9/Observables/PressureTrendSensor.java
+++ src/HW9/Observables/PressureTrendSensor.java
@@ -6,12 +6,12 @@ import Observers.Observer;
 public class PressureTrendSensor extends Observable implements Observer {
     // saves the last three times the pressure changes and tells us if it is rising
     // falling or stable
-    private int mostRecent;
-    private int secondMostRecent;
-    private int thirdMostRecent;
+    private int mostRecent = 0;
+    private int secondMostRecent = 0;
+    private int thirdMostRecent = 0;
     private String type = "pressure trend";
     private Trend pressureState;
-    private Trend lastState;
+    private Trend lastState = Trend.STABLE;
 
     public enum Trend {
         RISING, FALLING, STABLE
